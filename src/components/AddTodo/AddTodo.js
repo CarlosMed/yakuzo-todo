880c34a6d37b5a4f61e95ac2254708fe9f0b42df
Carlos Medina <carlos.w.med@gmail.com>
2019-02-03 23:40:07 -0500

    refactor: name change import of helpers to util

--- src/components/AddTodo/AddTodo.js
+++ src/components/AddTodo/AddTodo.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-import { db } from '../../helpers/firebase';
+import { db } from '../../utils/firebase';
 import './AddTodo.css';
 
 
